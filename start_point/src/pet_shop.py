bfbcb7ee31b7810f2a4e7ac88bbf86682aeff9e4
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-07-25 13:39:47 +0100

    defined function to get stock count

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -15,3 +15,6 @@ def get_pets_sold(pet_shop):
 
 def increase_pets_sold(pet_shop, number_sold):
     pet_shop["admin"]["pets_sold"] += number_sold
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
