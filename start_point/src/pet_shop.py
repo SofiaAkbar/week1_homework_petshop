87a94ae247a0c32467aadd91f2326de752fe7852
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-07-25 13:33:53 +0100

    defined function for pets sold and increase pets sold

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -10,3 +10,8 @@ def get_total_cash(pet_shop):
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
 
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, number_sold):
+    pet_shop["admin"]["pets_sold"] += number_sold
