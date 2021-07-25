9c26931305949c1df5606426b16f6ab2b9aaf590
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-07-25 13:22:09 +0100

    defined function to add or remove cash

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -5,3 +5,8 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+# function below works for both adding or removing cash
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
+
